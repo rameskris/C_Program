9f02e955869836f81232d6d51f2d63e0daa9e85e
rameskris <redmiblack2@gmail.com>
2022-08-15 13:45:43 +0530

    increment post and after

--- increment.c
+++ increment.c
@@ -6,13 +6,16 @@ int main()
 
     int a, b;
     a = 0;
-    b = a++;
+    b = a++ * 2 + 1; // first 'a' value zero is assigned to b then 'a' increment and asiigned to a
+                     // after *2 +1 happens to b hero zer0*2+1
+
     printf("After Post Increment\n");
     printf("a:%d\n", a);
     printf("b:%d\n", b);
 
     a = 0;
-    b = ++a;
+    b = ++a * 2 + 1;
+
     printf("After Post Increment\n");
     printf("a:%d\n", a);
     printf("b:%d\n", b);
