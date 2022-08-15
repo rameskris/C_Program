e96c8b273e5c35a723cab0ab73e8457faac5cd0b
rameskris <redmiblack2@gmail.com>
2022-08-15 12:50:00 +0530

    operator

--- bun_con.c
+++ bun_con.c
@@ -10,11 +10,17 @@ int main()
     const double speedofLight = 299792458.;
     const double pi = 3.142;
     const unsigned hexaDead = 0xDEADU;
-    const unsigned heaxaSecret = 519966U;
+    const unsigned heaxaSecret = 51966U;
 
     printf("Padded with spaces and padded with zeros");
 
     printf("Moon landing:\n %10d\n %010d\n", moonLanding, moonLanding);
+
+    printf("spped of light \n %.2f\n %.3e\n", speedofLight, speedofLight);
+
+    printf("Hexa Dead(here Captial x)\n 0x%X\n %6u\n\n", hexaDead, hexaDead);
+
+    printf("Heaxa Secret %x\n", heaxaSecret);
 
     return EXIT_SUCCESS;
 }
